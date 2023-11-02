de2120a769da71ade45571cdaf998ff7a4ba61f4
Hanif Faiz Hidayat <grey.hanif@gmail.com>
2023-11-02 20:00:38 +0700

    membuat interface

---
+++ src/programmer/zaman/now/data/Avanza.java
@@ -0,0 +1,13 @@
+package programmer.zaman.now.data;
+
+public class Avanza implements Car{
+
+
+    public void drive() {
+        System.out.println("Avanza drive");
+    }
+
+    public int getTier(){
+        return 4;
+    }
+}
