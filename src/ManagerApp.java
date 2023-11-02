eed00536b47efe0433ea6e982e955e1f4802ca8f
Hanif Faiz Hidayat <grey.hanif@gmail.com>
2023-11-02 17:52:45 +0700

    membuat super constructor

--- src/ManagerApp.java
+++ src/ManagerApp.java
@@ -1,11 +1,9 @@
 public class ManagerApp {
     public static void main(String[] args) {
-        var manager = new Manager();
-        manager.name = "Hanif";
+        var manager = new Manager("Hanif");
         manager.sayHello("Haikal");
 
-        var vp = new VicePresident();
-        vp.name = "Haikal";
+        var vp = new VicePresident("Haikal");
         vp.sayHello("Hanif");
     }
 }
--- src/VicePresident.java
+++ src/VicePresident.java
@@ -1,4 +1,8 @@
 public class VicePresident extends Manager{
+
+    VicePresident(String name){
+        super(name);
+    }
     void sayHello(String name){
         System.out.println("Hi " + name + ", My Name is VP " + this.name);
     }
