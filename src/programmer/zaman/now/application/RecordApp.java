b4ed921ecd0640a9c65be75183d6a03f97dc5135
Hanif Faiz Hidayat <grey.hanif@gmail.com>
2023-11-03 00:36:13 +0700

    membuat record class

---
+++ src/programmer/zaman/now/application/RecordApp.java
@@ -0,0 +1,17 @@
+package programmer.zaman.now.application;
+
+import programmer.zaman.now.data.LoginRequest;
+
+public class RecordApp {
+    public static void main(String[] args) {
+        LoginRequest loginRequest = new LoginRequest("hanif", "rahasia");
+
+        System.out.println(loginRequest.username());
+        System.out.println(loginRequest.password());
+        System.out.println(loginRequest);
+
+        System.out.println(new LoginRequest());
+        System.out.println(new LoginRequest("hanif"));
+        System.out.println(new LoginRequest("hanif", "rahasia"));
+    }
+}
