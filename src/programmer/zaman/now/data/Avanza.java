6f855cb03de7c8d9de0a9000c4f1a68e12a175bb
Hanif Faiz Hidayat <grey.hanif@gmail.com>
2023-11-02 22:00:52 +0700

    membuat interface inheritance

--- src/programmer/zaman/now/data/Avanza.java
+++ src/programmer/zaman/now/data/Avanza.java
@@ -10,4 +10,14 @@ public class Avanza implements Car{
     public int getTier(){
         return 4;
     }
+
+
+    public String getBrand() {
+        return "Toyota";
+    }
+
+
+    public boolean isMaintenance() {
+        return false;
+    }
 }
