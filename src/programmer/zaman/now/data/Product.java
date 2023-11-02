bb835d3eddc5ce30aa597cf28ea9bb8a47058a1e
Hanif Faiz Hidayat <grey.hanif@gmail.com>
2023-11-02 22:33:57 +0700

    membuat hashCode() method

--- src/programmer/zaman/now/data/Product.java
+++ src/programmer/zaman/now/data/Product.java
@@ -17,6 +17,13 @@ public class Product {
     }
 
     @Override
+    public int hashCode() {
+        int result = name != null ? name.hashCode() : 0;
+        result = 31 * result + price;
+        return result;
+    }
+
+    @Override
     public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
