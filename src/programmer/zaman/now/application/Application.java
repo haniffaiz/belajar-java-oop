bd943a102c69de6fc1d84e07e9b5b809e04bf77d
Hanif Faiz Hidayat <grey.hanif@gmail.com>
2023-11-02 19:23:29 +0700

    membuat import

--- src/programmer/zaman/now/application/Application.java
+++ src/programmer/zaman/now/application/Application.java
@@ -1,11 +1,14 @@
 package programmer.zaman.now.application;
 
-import programmer.zaman.now.data.Product;
+import programmer.zaman.now.data.*;
+
 
 public class Application {
     public static void main(String[] args) {
         Product product = new Product("Mac book Pro", 30000000);
         System.out.println(product.name);
         System.out.println(product.price);
+
+        Data data = new Data();
     }
 }
