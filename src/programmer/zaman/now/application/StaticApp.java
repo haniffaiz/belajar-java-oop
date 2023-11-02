10e8657fc1fd1b615bd7bde42ee281715f71bb0d
Hanif Faiz Hidayat <grey.hanif@gmail.com>
2023-11-03 00:19:33 +0700

    membuat static keyword

---
+++ src/programmer/zaman/now/application/StaticApp.java
@@ -0,0 +1,23 @@
+package programmer.zaman.now.application;
+
+import programmer.zaman.now.data.Application;
+import programmer.zaman.now.data.Constant;
+import programmer.zaman.now.data.Country;
+import programmer.zaman.now.util.MathUtil;
+
+public class StaticApp {
+    public static void main(String[] args) {
+        System.out.println(Constant.APPLICATION);
+        System.out.println(Constant.VERSION);
+        System.out.println(
+                MathUtil.sum(1,1,1,1,1)
+        );
+
+        Country.City city = new Country.City();
+        city.setName("Cilacap");
+
+        System.out.println(city.getName());
+
+        System.out.println(Application.PROCESSORS);
+    }
+}
